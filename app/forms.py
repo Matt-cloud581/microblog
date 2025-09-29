2242f682f3e483e04c9551d440890aabd32e191c
Matthew <mgareau1@hcc-nd.edu>
2025-09-29 23:10:21 +0000

    chapter7

--- app/forms.py
+++ app/forms.py
@@ -37,3 +37,15 @@ class EditProfileForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
     submit = SubmitField('Submit')
+    
+    def init(self, original_username, *args, **kwargs):
+        super().init(*args, **kwargs)
+        self.original_username = original_username
+        
+    def validate_username(self, username):
+        if username.data != self.original_username:
+            user = db.session.scalar(sa.select(User).where(
+                User.username == username.data))
+            if user is not None:
+                raise ValidationError('Please use a different username.')
+            
